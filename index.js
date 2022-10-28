b7019128eda9807dd156e53e0e6ba697e16a165c
rikeshhh <70895896+rikeshhh@users.noreply.github.com>
2022-10-29 01:05:41 +0900

    created a event listener to display user input

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const userInput = document.getElementById('userInput');
 const userBtn = document.getElementById('userBtn');
 const userInputCon = document.getElementById('userInputCon');
+
+userBtn.addEventListener('click',()=>{
+    let parentDiv = document.createElement('div');
+    let list = document.createElement('li');
+    list.innerText=`${userInput.value}`;
+    parentDiv.append(list);
+    userInputCon.append(parentDiv);
+})
