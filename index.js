f13db691ab60be1f521ab02e2fd4a07b64a4e8e5
rikeshhh <70895896+rikeshhh@users.noreply.github.com>
2022-10-29 03:22:38 +0900

    ending touches

--- index.js
+++ index.js
@@ -3,33 +3,38 @@ const userBtn = document.getElementById('userBtn');
 const userInputCon = document.getElementById('userInputCon');
 
 userBtn.addEventListener('click',()=>{
-    let parentDiv = document.createElement('div');
-    let list = document.createElement('li');
-    list.innerText=`${userInput.value}`;
-    userInput.value="";
-    parentDiv.append(list);
-    userInputCon.append(parentDiv);
-
-    let editBtn = document.createElement('button');
-    editBtn.innerText ="Edit";
-    let deleteBtn = document.createElement('button');
-    deleteBtn.innerText= "Delete";
-    parentDiv.append(editBtn,deleteBtn);
-
-    deleteBtn.addEventListener('click',()=>{
-        parentDiv.remove();
-    })
-    editBtn.addEventListener('click',(e)=>{
-        e.preventDefault();
-        if(editBtn.innerText==="Edit"){
-            document.getElementById('userBtn').style.display ="none";
-            const childInput = document.getElementById('userInput');
-            childInput.focus();
-            editBtn.innerText="Save";
-        }else{
-            list.innerText=`${userInput.value}`;
-            userInput.value="";
-            editBtn.innerText="Edit";
-        }
-    })
+    if(userInput.value===""){
+        alert("You have to insert something");
+    }else{
+        let parentDiv = document.createElement('div');
+        let list = document.createElement('li');
+        list.innerText=`${userInput.value}`;
+        userInput.value="";
+        parentDiv.append(list);
+        userInputCon.append(parentDiv);
+    
+        let editBtn = document.createElement('button');
+        editBtn.innerText ="Edit";
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerText= "Delete";
+        parentDiv.append(editBtn,deleteBtn);
+    
+        deleteBtn.addEventListener('click',()=>{
+            parentDiv.remove();
+        })
+        editBtn.addEventListener('click',(e)=>{
+            e.preventDefault();
+            if(editBtn.innerText==="Edit"){
+                const childInput = document.getElementById('userInput');
+                childInput.focus();
+                editBtn.innerText="Save";
+                
+            }else{
+                list.innerText=`${userInput.value}`;
+                userInput.value="";
+                editBtn.innerText="Edit";
+            }
+        })
+    }
+   
 })
